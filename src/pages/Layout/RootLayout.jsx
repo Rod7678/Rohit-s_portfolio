245a71a32b211952199d660ad752758ef55d9693
Rohit <rohitghadge348@gmail.com>
2026-06-28 13:03:43 +0530

    add Navbar and Footer components to RootLayout for improved site structure

--- src/pages/Layout/RootLayout.jsx
+++ src/pages/Layout/RootLayout.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router";
+import Navbar from "../../components/navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
 
 const RootLayout = () => {
   return (
     <>
+      <Navbar />
       <Outlet />
+      <Footer />
     </>
   );
 };
